73c44a77facf7fcbe03fc5940d7de04546cb666c
Nikolas Andronopoulos <nikolas.andronopoulos@gmail.com>
2017-01-26 22:28:50 +0200

    Version

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  * Created by Nikola Andronopoulos on 09/01/2017.
  *
  * Created for Geotag Aeroview CDN, cause why Akamai?
+ * @version 0.2.0
  */
 
 'use strict';
@@ -11,23 +12,22 @@
 // Fix errors if the script starts from another path
 process.chdir(__dirname);
 
-// Requires and what not
-const lineReader = require('readline').createInterface({
-    input: process.stdin
-});
 const path = require('path');
 const fs = require('fs');
 const url = require('url');
 const execSync = require('child_process').execSync;
-const masterPlaylistRenderer = require('./lib/masterM3u8Generator');
+const ffmpeg = require('fluent-ffmpeg');
+const m3u = require('m3u');
+const async = require('async');
 
 // Paths and arguments
-const inputPrefix = process.argv[2];
-const outputPrefix = process.argv[3];
-const cdnUrlPrefix = process.argv[4] || 'http://localhost:3000/';
+const inputFile = process.argv[2];
+const inputPrefix = process.argv[3] || '';
+const outputPrefix = process.argv[4] || '';
+const cdnUrlPrefix = process.argv[5] || 'http://localhost:3000/';
 const m3u8Prefix = 'index', m3u8Postfix = '.m3u8';
-let processedDirs = [];
 
+const lineReader = require('line-reader');
 
 /**
  * This function renders the arguments for the ffmpeg lib.
@@ -40,6 +40,7 @@ let processedDirs = [];
 const renderFFmpegArgs = (input, url, playlist, tsPath) => {
     let ffmpegArgs = `-i ${input} \
 -vcodec copy \
+-acodec copy \
 -vbsf:v h264_mp4toannexb \
 -start_number 0 \
 -hls_time 10 \
@@ -84,55 +85,90 @@ const getM3u8Name = (file) => {
 const setup = () => {
     if (!fs.existsSync(outputPrefix)) {
         fs.mkdirSync(outputPrefix);
-        process.stderr.write('Path was not found, created.\n');
+        console.log('Path was not found, created.');
     } else {
-        process.stderr.write('Path Exists.\n');
+        console.log('Path Exists.');
     }
 };
 
-/**
- * Main ts & m3u8 generation 'loop'.
- * Parses input line by line create directories on output and generates TS files with m3u8 playlists.
- */
-let renderTsFiles = () => {
-    let inputFilePath, m3u8Path, tsFullPath, currentOutDir, cdnUrl;
+setup();
 
-    lineReader.on('line', (line) => {
+/////////////////////////////////////////////////// MAGIC ///////////////////////////////////////////////////
+
+//noinspection JSUnresolvedFunction
+lineReader.eachLine(inputFile, (dirPath, last, nextLine) => {
+    console.log(`Current line: ${dirPath}`);
+    let inputDirPath = path.join(inputPrefix, dirPath);
+    let files = fs.readdirSync(inputDirPath);
+
+    // Master playlist reset...
+    let writer = m3u.httpLiveStreamingWriter();
+    writer.allowCache(true);
+
+    let foundVideosInPath = false;
+
+    async.forEachOfSeries(files, (file, idx, nextIteration) => {
         // Quick skip
-        if (path.extname(line) !== '.mp4') {
-            return;
+        if (path.extname(file) !== '.mp4') {
+            return nextIteration();
         }
 
+        // I found some :D
+        foundVideosInPath = true;
+
         // Oh noes variables
-        inputFilePath = path.join(inputPrefix, line);
-        cdnUrl = url.resolve(cdnUrlPrefix, path.join(path.dirname(line), 'chunks')) + '/';
-        currentOutDir = path.join(outputPrefix, path.dirname(line));
-        m3u8Path = path.join(currentOutDir, getM3u8Name(path.basename(line)));
-        tsFullPath = path.join(currentOutDir, 'chunks', path.basename(line.replace(path.extname(line), '-%03d.ts')));
+        let inputFilePath = path.join(inputDirPath, file);
+        let cdnUrl = url.resolve(cdnUrlPrefix, path.join(dirPath, 'chunks')) + '/';
+        let currentOutDir = path.join(outputPrefix, dirPath);
+        let chunksDir = path.join(currentOutDir, 'chunks');
+        let m3u8Path = path.join(currentOutDir, getM3u8Name(file));
+        let tsFullPath = path.join(chunksDir, path.basename(file.replace(path.extname(file), '-%03d.ts')));
 
-        // Put it in the Set for future use
-        processedDirs.push({
-            videoFile: inputFilePath,
-            outputPath: currentOutDir,
-            playlistPath: m3u8Path,
-            url: url.resolve(cdnUrlPrefix, path.dirname(line)) + '/'
-        });
+        // Dir creation skipping...
+        if (!fs.existsSync(chunksDir)) {
+            // Create the directories cause ffmpeg doesn't for some reason.
+            execSync(`mkdir -p ${chunksDir}`);
+        }
 
-        // Create the directories cause ffmpeg doesn't for some reason.
-        execSync(`mkdir -p ${path.join(currentOutDir, 'chunks')}`);
-        process.stderr.write(`Input is ${inputFilePath}\nM3u8 is ${m3u8Path}\nTsPath is ${tsFullPath}\nCDN is ${cdnUrl}\n`);
+        // Le debug
+        console.log(`InputFilePath: ${inputFilePath}`);
+        console.log(`ChunksDir: ${chunksDir}`);
+        console.log(`CDN Url: ${cdnUrl}`);
+        console.log(`CurrentOutDir: ${currentOutDir}`);
+        console.log(`m3u8Path: ${m3u8Path}`);
+        console.log(`TSFullPath: ${tsFullPath}`);
+
         // Go go power rangers!
         runFFmpeg(renderFFmpegArgs(inputFilePath, cdnUrl, m3u8Path, tsFullPath));
-    });
 
-    lineReader.on('close', () => {
-        process.stderr.write('Finished\n');
-        console.dir(processedDirs);
-        // Create the master playlist
-        masterPlaylistRenderer.generateFiles(processedDirs);
-    });
-};
+        // Render the master playlist from original video data
+        //noinspection JSUnresolvedFunction
+        ffmpeg.ffprobe(inputFilePath, (err, metadata) => {
+            //noinspection JSUnresolvedVariable
+            let masterPlaylistOptions = {
+                bandwidth: metadata.format.bit_rate,
+                programId: 1,
+                codecs: ['avc1.66.30', 'mp4a.40.5'],
+                resolution: metadata.streams[0].coded_width + 'x' + metadata.streams[0].coded_height
+            };
+            writer.playlist(url.resolve(url.resolve(cdnUrlPrefix, path.join(dirPath)) + '/', path.basename(m3u8Path)), masterPlaylistOptions);
+            nextIteration();
+        });
+    }, (error) => {
+        if (error) {
+            throw error;
+        }
+        // Check for empty list
+        if (foundVideosInPath) {
+            // Write the damn master playlist...
+            let currentOutDir = path.join(outputPrefix, dirPath);
+            fs.writeFileSync(path.join(currentOutDir, 'master.m3u8'), writer.toString());
+            console.log('Playlist:\n', writer.toString(), '\n##############################################\n');
+        }
 
-// Go go
-setup();
-renderTsFiles();
+        // You did?
+        foundVideosInPath = false;
+        // Next!
+        nextLine();
+    });
+});
